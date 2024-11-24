dd918edd7a734e81cccce9029b744a5a960089e1
zyyx-toandv <toandv@zyyx.jp>
2024-11-24 19:21:36 +0700

    update

--- predict_fire.py
+++ predict_fire.py
@@ -4,5 +4,5 @@ from ultralytics import YOLO
 model = YOLO("best.pt")
 
 # Perform object detection on an image
-results = model("fire_images/0000.jpg")
+results = model.predict("fire_images/0000.jpg", save=True)
 results[0].show()
